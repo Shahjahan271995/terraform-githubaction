6ec0e3f58b9ef6fd46de58ce17a0c51e4a3c827c
Md Shahjahan <mdshahjahan95@outlook.com>
2025-02-07 23:23:48 +0530

    rgadded

---
+++ main.tf
@@ -0,0 +1,15 @@
+
+#Resource Group in the second subscription
+resource "azurerm_resource_group" "githubrg" {
+
+ name     = "Actionrg"
+  location = "east us"
+}
+
+provider "azurerm" {
+  subscription_id = "3ba70bac-d8e7-4f2b-8e18-7fa19d2fb0f1"
+  features {
+    
+  }
+  
+}

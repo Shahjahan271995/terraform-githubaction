360d14788438091e6d30121a99a401a6649f4f1b
Md Shahjahan <mdshahjahan95@outlook.com>
2025-05-13 23:32:58 +0530

    code updated

--- providers.tf
+++ providers.tf
@@ -1,4 +1,11 @@
 terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.28.0"
+    }
+  }
+
   backend "azurerm" {
     resource_group_name  = "rajan"
     storage_account_name = "pipelinestorageaccoun4"
@@ -6,8 +13,9 @@ terraform {
     key                  = "terraform.tfstate"
   }
 }
+
 provider "azurerm" {
-  features {}
-  subscription_id = "0cead613-b99d-4057-9b73-e282208bb4c4"
+ subscription_id = "0cead613-b99d-4057-9b73-e282208bb4c4"
+ features {
+ }
 }
- 

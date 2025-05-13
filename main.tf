4c3052c662dfc652c965427f686c286320ce341c
Md Shahjahan <mdshahjahan95@outlook.com>
2025-05-13 22:44:01 +0530

    rg name changed

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "azurerm" {
   }
 }
 resource "azurerm_resource_group" "githubrg" {
-  name     = "vmrg"
+  name     = "mdrg"
   location = "east us"
 }
 resource "azurerm_public_ip" "exampleip" {

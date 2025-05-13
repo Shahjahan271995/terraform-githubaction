68b7d60890a76c4984806c9b689a09e92e4ed7a3
Md Shahjahan <mdshahjahan95@outlook.com>
2025-05-13 23:22:09 +0530

    rg added

--- main.tf
+++ main.tf
@@ -1,41 +1,52 @@
+provider "azurerm" {
+  features {}
+}
+
 resource "azurerm_resource_group" "githubrg" {
   name     = "githubactionrg"
   location = "east us"
 }
+
+resource "azurerm_virtual_network" "example_vnet" {
+  name                = "example-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.githubrg.location
+  resource_group_name = azurerm_resource_group.githubrg.name
+}
+
+resource "azurerm_subnet" "example_subnet" {
+  name                 = "example-subnet"
+  resource_group_name  = azurerm_resource_group.githubrg.name
+  virtual_network_name = azurerm_virtual_network.example_vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
 resource "azurerm_public_ip" "exampleip" {
   name                = "example-ip"
   location            = azurerm_resource_group.githubrg.location
   resource_group_name = azurerm_resource_group.githubrg.name
   allocation_method   = "Static"
 }
-resource "azurerm_network_interface" "example-nic" {
+
+resource "azurerm_network_interface" "example_nic" {
   name                = "example-nic"
   location            = azurerm_resource_group.githubrg.location
   resource_group_name = azurerm_resource_group.githubrg.name
+
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = azurerm_subnet.example-subnet.id
+    subnet_id                     = azurerm_subnet.example_subnet.id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.exampleip.id
   }
 }
-resource "azurerm_virtual_network" "example-vnet" {
-  name                = "example-vnet"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.githubrg.location
-  resource_group_name = azurerm_resource_group.githubrg.name
-}
-resource "azurerm_subnet" "example-subnet" {
-  name                 = "example-subnet"
-  resource_group_name  = azurerm_resource_group.githubrg.name
-  virtual_network_name = azurerm_virtual_network.example-vnet.name
-  address_prefixes     = ["10.0.2.0/24"]
-}
 
-resource "azurerm_network_security_group" "example-nsg" {
+resource "azurerm_network_security_group" "example_nsg" {
   name                = "example-nsg"
   location            = azurerm_resource_group.githubrg.location
-  resource_group_name = azurerm_resource_group.githubrg.name security_rule {
+  resource_group_name = azurerm_resource_group.githubrg.name
+
+  security_rule {
     name                       = "SSH"
     priority                   = 1001
     direction                  = "Inbound"
@@ -47,25 +58,30 @@ resource "azurerm_network_security_group" "example-nsg" {
     destination_address_prefix = "*"
   }
 }
-resource "azurerm_network_interface_security_group_association" "example-nic-nsg" {
-  network_interface_id      = azurerm_network_interface.example-nic.id
-  network_security_group_id = azurerm_network_security_group.example-nsg.id
+
+resource "azurerm_network_interface_security_group_association" "example_nic_nsg" {
+  network_interface_id      = azurerm_network_interface.example_nic.id
+  network_security_group_id = azurerm_network_security_group.example_nsg.id
 }
-# resource "azurerm_linux_virtual_machine" "example-vm" {
+
+# resource "azurerm_linux_virtual_machine" "example_vm" {
 #   name                            = "example-vm"
 #   resource_group_name             = azurerm_resource_group.githubrg.name
 #   location                        = azurerm_resource_group.githubrg.location
 #   size                            = "Standard_DS1_v2"
 #   admin_username                  = "adminuser"
-#   admin_password                  = "Password123"
+#   admin_password                  = "Password123!"
 #   disable_password_authentication = false
+
 #   network_interface_ids = [
-#     azurerm_network_interface.example-nic.id,
+#     azurerm_network_interface.example_nic.id,
 #   ]
+
 #   os_disk {
 #     caching              = "ReadWrite"
 #     storage_account_type = "Standard_LRS"
 #   }
+
 #   source_image_reference {
 #     publisher = "Canonical"
 #     offer     = "UbuntuServer"
--- providers.tf
+++ providers.tf
@@ -1,18 +1,13 @@
 terraform {
-    required_providers {
-        azurerm = {
-            source  = "hashicorp/azurerm"
-            version = "4.20.0"
-        }
-    }
-    backend "azurerm" {
-        resource_group_name  = "rajan"
-        storage_account_name = "pipelinestorageaccoun4"
-        container_name       = "pipelinecontainer"
-        key                  = "terraform.tfstate"
-    }
+  backend "azurerm" {
+    resource_group_name  = "rajan"
+    storage_account_name = "pipelinestorageaccoun4"
+    container_name       = "pipelinecontainer"
+    key                  = "terraform.tfstate"
+  }
 }
 provider "azurerm" {
-    features {}
-    subscription_id = "0cead613-b99d-4057-9b73-e282208bb4c4"
+  features {}
+  subscription_id = "0cead613-b99d-4057-9b73-e282208bb4c4"
 }
+ 
